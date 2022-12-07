6e994cc6620b720cc9b584d2b378bfb1a8ca1c08
leezhiwei <zhiweilee.com@gmail.com>
2022-12-07 09:50:22 +0800

    added PK to CustInfo table

--- sql/mssql/CreateCustInfoTable.sql
+++ sql/mssql/CreateCustInfoTable.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE SQLSyntaxTest; -- create db
 USE SQLSyntaxTest -- use the db
 GO
-CREATE TABLE CustInfo (CustID int not null, Name varchar(max) not null, Age int not null, IsCitizen BIT not null); -- create the table
+CREATE TABLE CustInfo (CustID int not null primary key, Name varchar(max) not null, Age int not null, IsCitizen BIT not null); -- create the table
 INSERT INTO CustInfo VALUES (1,'Dan',17,1),(2,'Mike',16,1),(3,'Micheal',20,0),(4,'Dennis',22,1); -- insert value
 SELECT * FROM CustInfo; -- show initial table
 ALTER Table CustInfo ADD DidBuyThings BIT not null DEFAULT 1; -- add column 
