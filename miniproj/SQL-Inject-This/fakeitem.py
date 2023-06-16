9d8a57181cfa8da92246c2412a74de3b70c44494
Lee Zhi Wei <s10239812@connect.np.edu.sg>
2023-06-16 21:42:40 +0800

    Remove SQLInjectThis

--- miniproj/SQL-Inject-This/fakeitem.py
+++
@@ -1,7 +0,0 @@
-from faker import Faker
-import faker_commerce
-fake = Faker() #faker object
-fake.add_provider(faker_commerce.Provider) #add commerce module
-def fakeitemname():
-    name = fake.ecommerce_name() # get random name of product
-    return name
